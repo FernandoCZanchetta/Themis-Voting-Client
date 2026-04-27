0c18329f002ff51da2344a4b1f8d1312d6b0cdf4
Fernando Cirilo Zanchetta <fe.cz@usp.br>
2026-04-27 19:32:34 -0300

    [Interceptors] Create Auth Interceptor to insert JWT in necessary requests

---
+++ src/app/interceptors/auth.interceptor.ts
@@ -0,0 +1,32 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http'
+import { Injectable } from '@angular/core'
+import { Observable } from 'rxjs'
+import { AuthService } from '@services'
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  constructor(private authService: AuthService) {}
+
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    const token = this.authService.getToken()
+
+    if (!token) {
+      return next.handle(req)
+    }
+
+    if (req.url.includes('/auth/login') || req.url.includes('audit')) {
+      return next.handle(req)
+    }
+
+    const authReq = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+
+    return next.handle(authReq)
+  }
+}
