1182150854b2990685c5cae38331add2a3f02d36
Fernando Cirilo Zanchetta <fe.cz@usp.br>
2026-04-27 20:04:12 -0300

    [Core] Create login component to allow users to authenticate into the website

---
+++ src/app/core/login/login.component.ts
@@ -0,0 +1,50 @@
+import { Component } from '@angular/core'
+import { FormControl, FormGroup, Validators } from '@angular/forms'
+import { ActivatedRoute, Router } from '@angular/router'
+import { AuthService } from '@services'
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent {
+  loading = false
+  error = ''
+
+  form = new FormGroup({
+    nUSP: new FormControl('', [Validators.required]),
+    uniquePassword: new FormControl('', [Validators.required]),
+  })
+
+  constructor(private authService: AuthService, private router: Router, private route: ActivatedRoute) {}
+
+  login() {
+    if (this.form.invalid) {
+      this.error = 'Preencha todos os campos!'
+      return
+    }
+
+    this.loading = true
+    this.error = ''
+
+    const { nUSP, uniquePassword } = this.form.value
+
+    this.authService.login(nUSP!, uniquePassword!).subscribe({
+      next: () => {
+        const returnUrl = this.route.snapshot.queryParamMap.get('returnUrl')
+
+        this.router.navigateByUrl(returnUrl || '/votings')
+      },
+
+      error: () => {
+        this.error = 'Falha no Login!'
+        this.loading = false
+      },
+
+      complete: () => {
+        this.loading = false
+      },
+    })
+  }
+}
