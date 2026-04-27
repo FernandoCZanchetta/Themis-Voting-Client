f3dc595b389e348994a5c0d5ee4bdc5d7ffc3a0c
Fernando Cirilo Zanchetta <fe.cz@usp.br>
2026-04-27 19:25:29 -0300

    [Services] Add Votings Service to handle votings get and vote post requests to API

---
+++ src/app/services/voting.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core'
+import { HttpClient } from '@angular/common/http'
+import { environment } from '../../environments/environment'
+import { VoteReceiptData, VoteRequestModel, VotingDetailsModel, VotingModel } from '@models'
+
+@Injectable({
+  providedIn: 'root',
+})
+export class VotingService {
+  private baseUrl = environment.baseUrl
+
+  constructor(private http: HttpClient) {}
+
+  getVotings(status?: 'active' | 'upcoming' | 'finished') {
+    return this.http.get<VotingModel[]>(
+      `${this.baseUrl}/voting`,
+      { params: status ? { status } : {} },
+    )
+  }
+
+  getVotingById(id: string) {
+    return this.http.get<VotingDetailsModel>(
+      `${this.baseUrl}/voting/${id}`
+    )
+  }
+
+  vote(votingId: string, data: VoteRequestModel) {
+    return this.http.post<VoteReceiptData>(
+      `${this.baseUrl}/vote/${votingId}`,
+      data
+    )
+  }
+}
