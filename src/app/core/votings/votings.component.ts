ef61700f55a2a5f760ae73b06fff5f5798dd6079
Fernando Cirilo Zanchetta <fe.cz@usp.br>
2026-04-27 19:55:11 -0300

    [Core] Create votings component to display the available votings

---
+++ src/app/core/votings/votings.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core'
+import { Router } from '@angular/router'
+import { VotingModel } from '@models'
+import { VotingService } from '@services'
+import { formatDate } from '@utils'
+
+@Component({
+  selector: 'app-votings',
+  templateUrl: './votings.component.html',
+  styleUrls: ['./votings.component.scss'],
+})
+export class VotingsComponent implements OnInit {
+  votings: VotingModel[] = []
+  loading = true
+  error = ''
+
+  formatDate = formatDate
+
+  constructor(private votingService: VotingService, private router: Router) {}
+
+  ngOnInit() {
+    this.loading = false
+    this.loadVotings()
+  }
+
+  loadVotings() {
+    this.loading = true
+    this.error = ''
+
+    this.votingService.getVotings('active').subscribe({
+      next: data => {
+        this.votings = data
+        this.loading = false
+      },
+      error: () => {
+        this.error = 'Erro ao carregar votações!'
+        this.loading = false
+      },
+    })
+  }
+
+  openVoting(id: string) {
+    this.router.navigate(['/voting', id])
+  }
+}
