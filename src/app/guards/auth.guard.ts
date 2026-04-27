5477bfaa4b1d0d3c2b234173f45888ac37049a75
Fernando Cirilo Zanchetta <fe.cz@usp.br>
2026-04-27 19:37:49 -0300

    [Guards] Create Auth Guard to protect authenticaded-acess-only routes

---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core'
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router'
+import { AuthService } from '@services'
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanActivate {
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    const isAuthenticated = this.authService.isAuthenticated()
+
+    if (!isAuthenticated) {
+      this.router.navigate(['/login'], {
+        queryParams: { returnUrl: state.url },
+      })
+      return false
+    }
+
+    return true
+  }
+}
